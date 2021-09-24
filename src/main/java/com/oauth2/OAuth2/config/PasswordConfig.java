f9f59ea79fe4689b2a6b53ab835415dad0f92687
wooho <woohobi@gmail.com>
2021-09-25 01:48:01 +0900

    encoder 추가

---
+++ src/main/java/com/oauth2/OAuth2/config/PasswordConfig.java
@@ -0,0 +1,14 @@
+package com.oauth2.OAuth2.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class PasswordConfig {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
