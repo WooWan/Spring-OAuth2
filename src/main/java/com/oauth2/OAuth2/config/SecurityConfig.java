1d59682e1d5590598611a898a6f8610351cf24fd
wooho <woohobi@gmail.com>
2021-09-24 16:19:52 +0900

    h2 db 설정

--- src/main/java/com/oauth2/OAuth2/config/SecurityConfig.java
+++ src/main/java/com/oauth2/OAuth2/config/SecurityConfig.java
@@ -1,31 +1,39 @@
 package com.oauth2.OAuth2.config;
 
+import com.oauth2.OAuth2.jwt.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 
+@RequiredArgsConstructor
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.headers().frameOptions().disable();
         http
+                .csrf().disable()
                 .cors()
                 .and()
                 .httpBasic().disable()
                 .formLogin().disable()
-                .csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
+                .addFilter(new JwtAuthenticationFilter(authenticationManager()))
                 .authorizeRequests()
-                .antMatchers("/", "/error", "/webjars/**").permitAll()
+                .antMatchers("/", "/error", "/webjars/**", "/h2-console/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .oauth2Login()
-                .and()
-                .logout()
-                .logoutSuccessUrl("/").permitAll();
+                .userInfoEndpoint()
+                .userService(customOAuth2UserService);
     }
 }
